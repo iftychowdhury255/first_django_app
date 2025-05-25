5d841bc9458fe26832f5e29141e0bf145c4c6ee3
iftychowdhury255 <iftychowdhury10212089@gmail.com>
2025-05-26 02:35:12 +0600

    data showing frondend home page

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add-data/', views.add_data, name='add_data')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import User
 
 def home(request):
     data = User.objects.all()
-    
     context = {
-        'data' : data
+        'data': data
     }
-    
     return render(request, 'home.html', context)
 
-# Create your views here.
+def add_data(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        age = request.POST.get('age')
+        isMaried = request.POST.get('isMaried') == 'on'  # checkbox returns 'on' if checked
+
+        user_data = User(
+            name=name,
+            email=email,
+            address=address,
+            age=age,
+            isMaried=isMaried
+        )
+
+        user_data.save()
+        messages.success(request, 'Data sent successfully!')
+        return redirect('add_data')
+
+    return render(request, 'add_data.html')
